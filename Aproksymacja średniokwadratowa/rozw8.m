c93880e3d6f36a225983244a5a30fd4de87981d6
ZuzannaJakubiak <zjakubiak20@gmail.com>
2024-10-01 15:20:25 +0200

    Add files via upload

--- Aproksymacja średniokwadratowa/rozw8.m
+++ Aproksymacja średniokwadratowa/rozw8.m
@@ -1,1 +1,17 @@
-
+x=-1:0.01:1
+y=x./(x.^2+2)
+a=polyfit(x,y,2)
+p=polyval(a,x) 
+bapr=y-p
+baprmax=max(abs(bapr))
+figure
+plot(x,y,x,p)
+title('aproksymacja funkcji')
+legend('funkcja', 'funkcja przybliżająca')
+xlabel('x')
+ylabel('y')
+figure
+plot(x,bapr)
+title('błąd aproksymacji')
+xlabel('x')
+ylabel('y') 
